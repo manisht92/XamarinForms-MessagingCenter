cdff8f4df9024ebb0bc538727a0be9c36868e36d
Manish Tiwari <manish.t92@gmail.com>
2018-12-30 09:42:42 +0530

    Finished

---
+++ XamarinMessagingCenter/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XamarinMessagingCenter
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            Navigation.PushAsync(new TargetPage());
+            MessagingCenter.Subscribe<TargetPage, double>(this, "SliderValueChanged", OnSliderValueChanged);
+        }
+
+        void OnSliderValueChanged(TargetPage source, double newValue)
+        {
+            label.Text = newValue.ToString();
+        }
+    }
+}
---
+++ XamarinMessagingCenter/TargetPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace XamarinMessagingCenter
+{
+    public partial class TargetPage : ContentPage
+    {
+        public TargetPage()
+        {
+            InitializeComponent();
+        }
+
+        void Handle_ValueChanged(object sender, Xamarin.Forms.ValueChangedEventArgs e)
+        {
+            MessagingCenter.Send(this, "SliderValueChanged", e.NewValue);
+        }
+    }
+}
